2a4d924c19c00ad7d8338bcf2c9066e0ca1d389f
chrisley <Chrisley.Oporta@ucr.ac.cr>
2024-11-15 12:12:13 -0600

    Update Proceso.java

--- Proyecto_Sistema Operativo de Pruebas/SistemaOperativo_Pruebas/Proceso.java
+++ Proyecto_Sistema Operativo de Pruebas/SistemaOperativo_Pruebas/Proceso.java
@@ -1,28 +1,51 @@
-
-
 class Proceso {
-    int id;
-    int tiempoLlegada;
-    int prioridad;
-    int tiempoCPU;
-    int memoriaRequerida;
-    int impresoras;
-    int escaneres;
-    int camara;
-    int parlantes;
-    int tiempoRestante;
-
-    Proceso(int id, int tiempoLlegada, int prioridad, int tiempoCPU, int memoriaRequerida,
-            int impresoras, int escaneres, int camara, int parlantes) {
-        this.id = id;
-        this.tiempoLlegada = tiempoLlegada;
-        this.prioridad = prioridad;
-        this.tiempoCPU = tiempoCPU;
-        this.memoriaRequerida = memoriaRequerida;
-        this.impresoras = impresoras;
-        this.escaneres = escaneres;
-        this.camara = camara;
-        this.parlantes = parlantes;
-        this.tiempoRestante = tiempoCPU;
+    
+        private int id;
+        private int prioridad;
+        private int tiempoCPURestante;
+        private int memoriaRequerida;
+        private int impresorasNecesarias;
+        private int escaneresNecesarios;
+    
+        public Proceso(int id, int prioridad, int tiempoCPU, int memoriaRequerida, int impresorasNecesarias, int escaneresNecesarios) {
+            this.id = id;
+            this.prioridad = prioridad;
+            this.tiempoCPURestante = tiempoCPU;
+            this.memoriaRequerida = memoriaRequerida;
+            this.impresorasNecesarias = impresorasNecesarias;
+            this.escaneresNecesarios = escaneresNecesarios;
+        }
+    
+        public int getId() {
+            return id;
+        }
+    
+        public int getPrioridad() {
+            return prioridad;
+        }
+    
+        public void setPrioridad(int prioridad) {
+            this.prioridad = prioridad;
+        }
+    
+        public int getTiempoCPURestante() {
+            return tiempoCPURestante;
+        }
+    
+        public void actualizarTiempoCPU(int tiempo) {
+            this.tiempoCPURestante -= tiempo;
+        }
+    
+        public int getMemoriaRequerida() {
+            return memoriaRequerida;
+        }
+    
+        public int getImpresorasNecesarias() {
+            return impresorasNecesarias;
+        }
+    
+        public int getEscaneresNecesarios() {
+            return escaneresNecesarios;
+        }
     }
-}
+    
